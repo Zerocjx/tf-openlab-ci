e36cc2a3f42a62af5f53794ed077f26a24de83b6
zerocjx <zerocjx@outlook.com>
2019-04-17 15:47:40 +0800

    add proxy for testing

--- build/build.sh
+++ build/build.sh
@@ -26,9 +26,13 @@ docker exec $DEV_CONTAINER_ID git --git-dir=$VROUTER_DIR/.git --work-tree=$VROUT
 docker exec $DEV_CONTAINER_ID rm -rf $CONTRAIL_DIR/build/production/vrouter/dpdk/contrail-vrouter-dpdk
 
 #TODO add proxy automatically
-#use -e option for passing proxy into containers
-docker exec $DEV_CONTAINER_ID make -C $DEV_ENV_DIR sync
+docker exec -e http_proxy=http://192.168.0.1:8123 -e https_proxy=http://192.168.0.1:8123 $DEV_CONTAINER_ID make -C $DEV_ENV_DIR sync
 
-docker exec $DEV_CONTAINER_ID scons -C /root/contrail -j 2 --opt=production
+docker exec -e http_proxy=http://192.168.0.1:8123 -e https_proxy=http://192.168.0.1:8123 $DEV_CONTAINER_ID scons -C /root/contrail -j 2 --opt=production
+
+#use -e option for passing proxy into containers
+# docker exec $DEV_CONTAINER_ID make -C $DEV_ENV_DIR sync
+
+# docker exec $DEV_CONTAINER_ID scons -C /root/contrail -j 2 --opt=production
 
 docker cp $DEV_CONTAINER_ID:$CONTRAIL_DIR/build/production/vrouter/dpdk/contrail-vrouter-dpdk .
